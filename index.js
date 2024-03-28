81cfb06fa3c0c218fad8cb9ee397592d432e7cc6
SirNazaryan <88489987+SirNazaryan@users.noreply.github.com>
2024-03-29 01:26:00 +0300

    Homework_3

--- index.js
+++ index.js
@@ -1,529 +1,85 @@
-// // Если переменная a равна нулю, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 1, 0, -3.//
-// const a = 1
-
-// if (a===0){
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = 0
-
-// if (a===0){
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = -3
-
-// if (a===0){
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-//2.Если переменная a больше нуля, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 1, 0, -3.//
-// const a = 1
-// if (a>0) {
-//     console.log ("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = 0
-// if (a>0) {
-//     console.log ("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = -3
-// if (a>0) {
-//     console.log ("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// 3. Если переменная a меньше нуля, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 1, 0, -3.
-//  const a = 1
-//  if(a<0) {
-//     console.log ("Верно")
-//  }
-//  else {
-//     console.log("Неверно")
-//  }
-
-//  const a = 0
-//  if(a<0) {
-//     console.log ("Верно")
-//  }
-//  else {
-//     console.log("Неверно")
-//  }
-
-//  const a = -3
-//  if(a<0) {
-//     console.log ("Верно")
-//  }
-//  else {
-//     console.log("Неверно")
-//  }
-
-// 4.Если переменная a больше или равна нулю, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 1, 0, -3.
-// const a = 1
-
-// if (a>=0) {
-//     console.log("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// const a = 0
-
-// if (a>=0) {
-//     console.log("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// const a = -3
-
-// if (a>=0) {
-//     console.log("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// 5. Если переменная a меньше или равна нулю, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 1, 0, -3.
-
-// const a = 1
-// if (a<=0) {
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = -3
-// if (a<=0) {
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = 0
-// if (a<=0) {
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// 6.  Если переменная a не равна нулю, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 1, 0, -3.
-
-// const a = 1
-
-// if (a != 0) {
-//     console.log ("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// const a = 0
-
-// if (a != 0) {
-//     console.log ("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// const a = -3
-
-// if (a != 0) {
-//     console.log ("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// 7.  Если переменная a равна 'test', то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 'test', 'тест', 3.
-
-// const a = "test"
-
-// if(a === "test") {
-//     console.log("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// const a = "test"
-
-// if(a === "тест") {
-//     console.log("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// const a = 3
-
-// if(a === "test") {
-//     console.log("Верно")
-// }
-// else {
-//     console.log ("Неверно")
-// }
-
-// 8. Если переменная a равна '1' и по значению и по типу, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном '1', 1, 3.
-
-// const a =1
-
-// if(a==="1"){
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = "1"
-
-// if(a==="1"){
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-// const a = 3
-
-// if(a==="1"){
-//     console.log("Верно")
-// }
-// else {
-//     console.log("Неверно")
-// }
-
-//Работа с логическими переменными//
-
-// 1. Если переменная test равна true, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при test, равном true, false. Напишите два варианта скрипта - с короткой записью и с длинной.
-
-// const test = true
-//  if(test == true) {
-//     console.log ("Верно")
-//  }
-//  else{
-//     console.log("Неверно")
-//  }
-// const test = true
-//  test == true ? console.log("Верно"): console.log("Неверно");
-
-//  const test = false
-//  if(test == true) {
-//     console.log ("Верно")
-//  }
-//  else{
-//     console.log("Неверно")
-//  }
-// const test = false
-//  test == true ? console.log("Верно"): console.log("Неверно");
-
-// 2.  Если переменная test не равна true, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при test, равном true, false. Напишите два варианта скрипта - с короткой записью и с длинной.
-
-// const test = true;
-
-// if (test != true){
-//     console.log("Верно")
-// }
-// else{
-//     console.log("Неверно")
-// }
-
-// const test = true;
-
-// test != true  ? console.log("Верно"): console.log("Неверно")
-
-
-// const test = false;
-
-// if (test != true){
-//     console.log("Верно")
-// }
-// else{
-//     console.log("Неверно")
-// }
-
-// const test = false;
-
-// test != true  ? console.log("Верно"): console.log("Неверно")
-
-
-//Работа с && (и) и || (или)
-
-// 1. Если переменная a больше нуля и меньше 5-ти, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при a, равном 5, 0, -3, 2.
-//  const a = 5;
-
-//  if(a>0 && a<5){
-//     console.log("Верно")
-//  }
-//  else {
-//     console.log("Неверно")
-//  }
-
-
-//  const a = 0;
-
-//  if(a>0 && a<5){
-//     console.log("Верно")
-//  }
-//  else {
-//     console.log("Неверно")
-//  }
-
-//  const a = -3;
-
-//  if(a>0 && a<5){
-//     console.log("Верно")
-//  }
-//  else {
-//     console.log("Неверно")
-//  }
-
-//  const a = 2;
-
-//  if(a>0 && a<5){
-//     console.log("Верно")
-//  }
-//  else {
-//     console.log("Неверно")
-//  }
-
-// 2.  Если переменная a равна нулю или равна двум, то прибавьте к ней 7, иначе поделите ее на 10. Выведите новое значение переменной на экран. Проверьте работу скрипта при a, равном 5, 0, -3, 2.
-// const a = 5;
-
-// if(a == 0  || a ==2){
-//     res = a + 7;
-//     console.log(res)
-// }
-// else{
-//     res = a/10
-//     console.log(res)
-// }
-
-// const a = 0;
-
-// if(a == 0  || a ==2){
-//     res = a + 7;
-//     console.log(res)
-// }
-// else{
-//     res = a/10
-//     console.log(res)
-// }
-
-// const a = -3;
-
-// if(a == 0  || a ==2){
-//     res = a + 7;
-//     console.log(res)
-// }
-// else{
-//     res = a/10
-//     console.log(res)
-// }
-
-// const a = 2;
-
-// if(a == 0  || a ==2){
-//     res = a + 7;
-//     console.log(res)
-// }
-// else{
-//     res = a/10
-//     console.log(res)
-// }
-
-// 3.  Если переменная a равна или меньше 1, а переменная b больше или равна 3, то выведите сумму этих переменных, иначе выведите их разность (результат вычитания). Проверьте работу скрипта при a и b, равном 1 и 3, 0 и 6, 3 и 5.
-// const a = 1
-// const b = 3
-
-// if(a <= 1 && b >= 3){
-//     console.log(a + b)
-// }
-// else{
-//     console.log(a-b)
-// }
-
-// const a = 0
-// const b = 6
-
-// if(a <= 1 && b >= 3){
-//     console.log(a + b)
-// }
-// else{
-//     console.log(a-b)
-// }
-
-// const a = 3
-// const b = 5
-
-// if(a <= 1 && b >= 3){
-//     console.log(a + b)
-// }
-// else{
-//     console.log(a-b)
-// }
-
-// 4. Если переменная a больше 2-х и меньше 11-ти, или переменная b больше или равна 6-ти и меньше 14-ти, то выведите 'Верно', в противном случае выведите 'Неверно'.
-
-// const a = 2
-// const b = 6
-
-// if(a > 2 && a < 11 || b >= 6 && b < 14){
-//     console.log ("Верно")
-// }
-// else{
-//     console.log("Неверно")
-// }
-
-
-//На switch-case
-// 1. Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет значение '1', то в переменную result запишем 'зима', если имеет значение '2' – 'весна' и так далее. Решите задачу через switch-case.
-// let num = 0;
-// switch (num){
-//     case 1:
-//         result = "Зима"
-//         console.log(result);
-//         break
-//     case 2:
-//         result = "Весна"
-//         console.log("result");
-//         break
-//     case 3:
-//         result = "Лето"
-//         console.log("result");
-//         break
-//     case 4:
-//         result = "Осень"
-//         console.log("result")
-//         break
-//     default:
-//         result = "Введите цифру от 1 до 4";
-//         break
-// }
-
-//Общие задачи
-
-// let day = 27
-
-// if(day >= 1 && day <= 10){
-//     console.log("Число попадает в первую декаду")
-// }
-// else if(day > 10 && day <=20){
-//     console.log("Число попадает во вторую декаду")
-// }
-// else if(day > 20 && day <= 31){
-//     console.log("Число попадает в третью декаду")
-// }
-// else {
-//     console.log("Наберите число от 1 до 31")
-// }
-
-
-// 2. В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
-// let month = 3
-
-// if(month >= 3 && month <= 5){
-//     console.log("Весна")
-// }
-// else if(month > 5 && month <= 8){
-//     console.log("Лето")
-// }
-// else if(month > 8 && month < 12){
-//     console.log("Осень")
-// }
-// else if(month == 12 || month == 1 || month == 2){
-//     console.log("Зима")
-// }
-// else{
-//     console.log("В году 12 месяцев, введите с 1 до 12")
-// }
-
-//Циклы while и for
-// Решите эти задачи сначала через цикл while, а затем через цикл for.
-
-// 1.  Выведите столбец чисел от 1 до 100.
-// for(i = 1; i<=100; i++){
-//     console.log(i)
-// }
-
-// while 
-// let num = 1;
-// while(num > 0 && num <= 100){
-//     console.log(num);
-//     num++;
-// }
-
-// 2. Выведите столбец чисел от 11 до 33.
-// for(i = 11; i <= 33; i++){
-//     console.log(i)
-// }
-// let num = 11;
-// while(num>= 11 && num <= 33){
-//     console.log(num);
-//     num++
-// }
-
-// 3.  Выведите столбец четных чисел в промежутке от 0 до 100.
-// for(i = 0; i <= 100; i++){
-//     if(i % 2 == 0){
-//         console.log(i)
-//     }
-// }
-
-// 4.  С помощью цикла найдите сумму чисел от 1 до 100.
-
-// let sum = 0;
-
-// for(i = 0; i <= 100; i++){
-//     sum = sum + i
-// }
-// console.log(sum)
-
-// function getDegree(a, b){
-//     return result = (a**b)
-// }
-// console.log(getDegree(2,1))
-
-// function checkNum(a){
-//     if(a%2==0){
-//         return "Четное"
-//     }
-//     else {
-//         return "Не четное"
-//     }
-// }
-// console.log(getNum(3))
-
-const checkVowel = (letter) => {
-    if (letter === "а" || letter === "e" || letter === "и" || letter === "о" || letter === "у" || letter === "э"){
-        return "гласная"
+1. Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.
+function getSquare(x) {
+    return x**2
+}
+ console.log(getSquare(6))
+
+2. Сделайте функцию, которая возвращает сумму двух чисел.
+
+function getSum(a,b) {
+    return (a+b)
+}
+console.log(getSum(3,4))
+
+3. Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
+
+function getMath(a, b, c) {
+    return ((a-b)/c)
+}
+console.log(getMath(5, 2, 3))
+
+4.Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
+
+function getDayOfWeek(day){
+    if(day === 1) {
+        console.log("Сегодня понедельник")
+    }
+    else if(day === 2) {
+        console.log("Сегодня вторник")
+    }
+    else if(day == 3) {
+        console.log("Сегодня среда")
+    }
+    else if(day == 4) {
+        console.log("Сегодня четверг")
+    }
+    else if(day == 5) {
+        console.log("Сегодня пятница")
+    }
+    else if(day == 6) {
+        console.log("Сегодня суббота")
+    }
+    else if(day == 7) {
+        console.log("Сегодня воскресенье")
     }
     else{
-        return "не гласная"
+        console.log("Наберите число от 1-7")
     }
 }
-console.log(checkVowel("а"))
+getDayOfWeek(3)
+
+5. Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+
+function getMathOper(x, y){
+    if(x===y){
+        console.log("true")
+    }
+    else{
+        console.log("false")
+    }
+}
+getMathOper(3,3)
+
+6. Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть вернет true, а если нет то - false.
+ function checkSum(a,b) {
+     return (a+b) > 10 ? "true": "false" 
+ }
+ console.log(checkSum(6,7))
+
+7. Сделайте функцию, которая параметром принимает число и проверяет - отрицательное оно или нет. Если отрицательное - пусть функция вернет true, а если нет - false.
+    function checkNum(a) {
+        return a < 0 ? "true" : "false"
+    }
+    console.log(checkNum(-10))
+
+    8. Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет, что оно больше нуля и меньше 10. Если это так - пусть функция возвращает true, если не так - false.
+    function isNumberInRange(x) {
+        return ((x > 0) && (x < 10)) ? "true": "false"
+    }
+    console.log(isNumberInRange(11))
+
+    9. Сделайте функцию isEven() (even - это четный), которая параметром принимает целое число и проверяет: четное оно или нет. Если четное - пусть функция возвращает true, если нечетное - false.
+        function isEven(x) {
+            return (x%2 === 0) ? "true" :"false"
+        }
+        console.log(isEven(4))
